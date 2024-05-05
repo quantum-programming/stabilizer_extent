fd8314cf28ac779187882d78b27d201703c156d8
hari64 <hari64boli64@gmail.com>
2024-05-05 17:55:11 +0900

    update C++ code and vscode settings

--- exputils/cpp/profile.sh
+++ exputils/cpp/profile.sh
@@ -4,9 +4,9 @@ path=./exputils/cpp
 filename=$path/calc_dot
 
 echo "now compiling..."
-/usr/bin/g++-10 -pg -g -fdiagnostics-color=always $filename.cpp -o $filename -std=c++17 -Wall -Wextra -O3 -lz
+/usr/bin/g++-10 -pg -g -fdiagnostics-color=always $filename.cpp -o $filename.exe -std=c++17 -Wall -Wextra -O2 -lz
 
 echo "now running..."
-$filename
-gprof $filename gmon.out > gprof.profile
+$filename.exe
+gprof $filename.exe gmon.out > gprof.profile
 rm gmon.out
