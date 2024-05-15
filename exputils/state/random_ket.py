b2dbc55b8d86bac2b0686344191e7cc3d7851268
hari64 <hari64boli64@gmail.com>
2024-05-15 14:48:00 +0900

    add real state

--- exputils/state/random_ket.py
+++ exputils/state/random_ket.py
@@ -6,18 +6,24 @@ def _make_random_pure_state(n: int, seed: int) -> np.ndarray:
     return qutip.rand_ket(2**n, seed=seed).full().flatten()
 
 
-def make_random_quantum_state(kind: str, n: int, seed: int):
-    assert kind in ["pure"], kind
+def make_random_quantum_state(kind: str, n: int, seed: int) -> np.ndarray:
+    assert kind in ["pure", "real"], kind
     if kind == "pure":
         ket = _make_random_pure_state(n, seed)
+    elif kind == "real":
+        ket = np.random.rand(2**n) - 0.5
+        ket = ket / np.linalg.norm(ket)
+        ket = ket.astype(np.complex128)
+
     return ket
 
 
 def main():
     for n in range(1, 5 + 1):
         for seed in range(5):
-            ket = make_random_quantum_state("pure", n, seed)
-            print(f"{n=}, {seed=}, {ket[:5]=}, {np.linalg.norm(ket)=}")
+            for kind in ["pure", "real"]:
+                ket = make_random_quantum_state(kind, n, seed)
+                print(f"{n=}, {seed=}, {kind=}, {ket[:5]=}, {np.linalg.norm(ket)=}")
 
 
 if __name__ == "__main__":
